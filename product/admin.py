296848cce38eb8bc7689e1298372a64157a08aba
Zohaib Khan <zohaibk1111@gmail.com>
2025-08-10 02:11:42 -0400

    added admin customerization

--- product/admin.py
+++ product/admin.py
@@ -3,8 +3,27 @@ from .models import Product, Order
 
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Order)
+# admin.site.register(Product)
+# admin.site.register(Order)
 admin.site.site_header = "E-commerce Admin"
 admin.site.site_title = "E-commerce Admin Portal"
 admin.site.index_title = "Welcome to the E-commerce Admin Portal"
+
+
+class ProductAdmin(admin.ModelAdmin):
+    def change_category_to_default(self, request, queryset):
+        queryset.update(category="default")
+    change_category_to_default.short_description="Change category to default"
+    list_display = ('name', 'price', 'quantity', 'category', 'discounted_price')
+    search_fields =('name', 'category')
+    actions=['change_category_to_default']
+    fields = ('name', 'price')
+    list_editable = ('price', 'quantity', 'category', 'discounted_price')
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'total_price', 'state')
+    search_fields = ('name', 'email')
+
+admin.site.register(Order, OrderAdmin)
+
+admin.site.register(Product, ProductAdmin)
